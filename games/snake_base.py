c15628aeb25dbacf4dcf29658761c0e7f185a9a5
Xuejun <xuejun.liu@shihengtech.com>
2023-10-09 18:14:44 +0800

    Add drop foods.

--- games/snake_base.py
+++ games/snake_base.py
@@ -97,16 +97,11 @@ class Plat(object):
             else:
                 plat[point[0]][point[1]] = -1
 
-    def init_foods(self):
-        if (
-            self.food_num <= 0
-            or self.food_num >= self.width * self.height - self.bomb_num
-        ):
-            raise Exception("food num is illegal.")
+    def supplement_food(self, food_num=1):
         plat = copy.deepcopy(self.plat)
         # Snake init coord
         plat[self.width // 2][self.height // 2] = -1
-        for _ in range(self.food_num):
+        for _ in range(food_num):
             if len(self.score_map) == 1:
                 food = PlatEnum.Food_MIDDLE.value
             elif len(self.score_map) == 2:
@@ -130,6 +125,14 @@ class Plat(object):
             plat[point[0]][point[1]] = 1
             self.plat[point[0]][point[1]] = food
 
+    def init_foods(self):
+        if (
+            self.food_num <= 0
+            or self.food_num >= self.width * self.height - self.bomb_num
+        ):
+            raise Exception("food num is illegal.")
+        self.supplement_food(self.food_num)
+
     def reset(self):
         # Clear the map
         self.plat[:] = PlatEnum.Ground.value
@@ -138,12 +141,32 @@ class Plat(object):
         # Initiate foods
         self.init_foods()
 
+    def get_food_num(self):
+        n = np.where(
+            (PlatEnum.Food_SMALL.value == self.plat)
+            | (PlatEnum.Food_MIDDLE.value == sb.plat.plat)
+            | (PlatEnum.Food_LARGE.value == sb.plat.plat)
+        )
+        if not n:
+            return 0
+        return n[0].size
+
 
 class Snake(object):
     def __init__(self, head):
-        self.head = head
+        self.init_head = head
+        self.head = copy.deepcopy(head)
         self.bodies = []
-        self.score = 0
+        self.score = []
+        self.terminal = False
+
+    def revive(self):
+        self.score = []
+        self.head = self.init_head
+        self.bodies = []
+
+    def over(self):
+        self.terminal = True
 
 
 class SnakeBase(object):
@@ -156,19 +179,24 @@ class SnakeBase(object):
         food_scores=(1, 2, 3),
         bomb_num=0,
         snake_num=1,
+        revive=False,
     ):
         self.width = width
         self.height = height
         self.food_scores = food_scores
         self.snake_num = snake_num
+        self.snake_init_coord = [width // 2, height // 2]
+        self.revive = revive
         self.snakes = {}
-        self.score_map = self.init_score_map()
+        self.food_score_map = self.init_score_map()
+        self.score_food_map = {v: k for k, v in self.food_score_map.items()}
+        self.food_num = food_num
         self.plat = Plat(
             width=width,
             height=height,
             food_num=food_num,
             max_food_total=max_food_total,
-            score_map=self.score_map,
+            score_map=self.food_score_map,
             bomb_num=bomb_num,
         )
         self.terminal = False
@@ -204,18 +232,27 @@ class SnakeBase(object):
         for i in range(self.snake_num):
             if self.snakes.get(i):
                 del self.snakes[i]
-        self.snakes = {
-            i: Snake([self.width // 2, self.height // 2]) for i in range(self.snake_num)
-        }
+        self.snakes = {i: Snake(self.snake_init_coord) for i in range(self.snake_num)}
         # Reset args
         self.terminal = False
 
+    def get_merge_snake_plat(self):
+        plat = copy.deepcopy(self.plat.plat)
+        for snake in self.snakes.values():
+            head = snake.head
+            plat[head[0]][head[1]] = PlatEnum.SnakeHead.value
+            for body in snake.bodies:
+                plat[body[0]][body[1]] = PlatEnum.SnakeBody.value
+        return plat
+
     def get_next_item(self, snake_head_x, snake_head_y, direction):
+        plat = self.get_merge_snake_plat()
+
         if direction == Direction.UP:
             if snake_head_x <= 0:
                 return PlatEnum.Outside.value, None
             return (
-                self.plat.plat[snake_head_x - 1][snake_head_y],
+                plat[snake_head_x - 1][snake_head_y],
                 [snake_head_x - 1, snake_head_y],
             )
 
@@ -223,7 +260,7 @@ class SnakeBase(object):
             if snake_head_x >= self.height - 1:
                 return PlatEnum.Outside.value, None
             return (
-                self.plat.plat[snake_head_x + 1][snake_head_y],
+                plat[snake_head_x + 1][snake_head_y],
                 [snake_head_x + 1, snake_head_y],
             )
 
@@ -231,7 +268,7 @@ class SnakeBase(object):
             if snake_head_y <= 0:
                 return PlatEnum.Outside.value, None
             return (
-                self.plat.plat[snake_head_x][snake_head_y - 1],
+                plat[snake_head_x][snake_head_y - 1],
                 [snake_head_x, snake_head_y - 1],
             )
 
@@ -239,11 +276,27 @@ class SnakeBase(object):
             if snake_head_y >= self.width - 1:
                 return PlatEnum.Outside.value, None
             return (
-                self.plat.plat[snake_head_x][snake_head_y + 1],
+                plat[snake_head_x][snake_head_y + 1],
                 [snake_head_x, snake_head_y + 1],
             )
         else:
             raise Exception(f"Unknown direct: {self.snake_direction}")
+
+    @staticmethod
+    def move(snake, next_coord, grown=False):
+        snake.bodies.insert(0, snake.head)
+        snake.head = next_coord
+        if not grown:
+            snake.bodies.pop()
+
+    def drop_foods(self, snake):
+        if not snake.bodies:
+            return
+        snake_list = [snake.head, *snake.bodies[:-1]]
+        score = snake.score
+        for i, body in enumerate(snake_list[: len(snake_list) // 2]):
+            if self.plat.plat[body[0]][body[1]] == PlatEnum.Ground.value:
+                self.plat.plat[body[0]][body[1]] = self.score_food_map[score[i]]
 
     def snake_move(self, snake_id, direction):
         if self.terminal:
@@ -254,83 +307,65 @@ class SnakeBase(object):
 
         snake_head_x, snake_head_y = snake.head
 
-        next_item, [next_head_x, next_head_y] = self.get_next_item(
+        next_item, next_coord = self.get_next_item(
             snake_head_x, snake_head_y, direction
         )
+
+        # The 1st joint
+        if next_item == PlatEnum.SnakeBody.value and next_coord == snake.bodies[0]:
+            # Ignore operation
+            return
+
         # Food
         if next_item in (
             PlatEnum.Food_SMALL.value,
             PlatEnum.Food_MIDDLE.value,
             PlatEnum.Food_LARGE.value,
         ):
-            snake.score += self.score_map[next_item]
-            # self.move()
-        # Snake body or Bomb
+            snake.score.append(self.food_score_map[next_item])
+            self.move(snake, next_coord, grown=True)
+            self.plat.plat[next_coord[0]][next_coord[1]] = PlatEnum.Ground.value
+            food_num = self.plat.get_food_num()
+            if food_num < self.food_num:
+                if not self.is_full():
+                    self.plat.supplement_food()
+            if food_num == 0:
+                # Win
+                self.terminal = True
+                return
+
+        # Snake body or Bomb or Outside
+        elif next_item in [PlatEnum.Bomb.value, PlatEnum.Outside.value] or (
+            next_item == PlatEnum.SnakeBody.value and next_coord != snake.bodies[-1]
+        ):
+            if self.revive:
+                self.drop_foods(snake)
+                snake.revive()
+            else:
+                snake.over()
+                self.terminal = True
+                return
 
         # Snake tail or Ground
+        elif next_item == PlatEnum.Ground.value or (
+            next_item == PlatEnum.SnakeBody.value and next_coord == snake.bodies[-1]
+        ):
+            self.move(snake, next_coord)
 
-        next_coord = self.snake[0]
-        for idx in range(1, len(self.snake)):
-            current_coord = self.snake[idx]
-            current_item = self.plat[current_coord[0]][current_coord[1]]
-            self.plat[next_coord[0]][next_coord[1]] = current_item
-            self.plat[current_coord[0]][current_coord[1]] = PlatEnum.Ground.value
-            self.snake[idx] = next_coord
-            next_coord = current_coord
-        if self.plat[self.snake[0][0], self.snake[0][1]] == PlatEnum.SnakeHead.value:
-            self.plat[self.snake[0][0], self.snake[0][1]] = PlatEnum.Ground.value
-        self.plat[item_coord[0], item_coord[1]] = PlatEnum.SnakeHead.value
-        self.snake[0] = item_coord
+        # TODO
+        self.print_plat()
 
-    def run_a_turn(self, snake_id, direction):
-        if this.terminal:
-            return
-        if not self.snakes.get(snake_id):
-            raise Exception("Unknown snake.")
-        snake = self.snakes[snake_id]
+    def print_plat(self):
+        plat = self.get_merge_snake_plat()
+        print(plat)
 
-        snake_head_x, snake_head_y = snake.head
-        # next_item, item_coord = self.get_next_item(snake_head_x, snake_head_y)
+    def is_full(self):
+        plat = self.get_merge_snake_plat()
+        ground = np.where(plat == 0)
+        if not ground:
+            return True
 
-        # Game over
-        if next_item in [
-            PlatEnum.Outside.value,
-            PlatEnum.SnakeHead.value,
-            PlatEnum.Bomb.value,
-        ]:
-            self.game_over()
-            return next_item
-        elif next_item == PlatEnum.SnakeBody.value:
-            # The tail
-            if item_coord == self.snake[-1]:
-                self.snake_move(item_coord)
-            else:
-                self.game_over()
-            return next_item
-        # Eat food
-        elif next_item == PlatEnum.Food.value:
-            self.eat_food(item_coord)
-            self.add_reward()
-            self.score += 1
-
-            # Add snake body
-            body_coord = self.snake[-1]
-            self.snake_move(item_coord)
-            self.create_snake_body(body_coord)
-            self.create_food()
-            return next_item
-        elif next_item == PlatEnum.Ground.value:
-            self.snake_move(item_coord)
-            new_distance = self.get_distance()
-            de_reward = 0
-            # if new_distance > distance:
-            #     de_reward *= 2
-            if new_distance < distance:
-                de_reward = 0
-            self.reduce_reward(value=de_reward)
-            return next_item
-        else:
-            raise Exception(f"Unknown plat type. {next_item}")
+        return ground[0].size <= 1
 
     def eat_food(self, food_coord):
         self.plat[food_coord[0]][food_coord[1]] = PlatEnum.Ground.value
@@ -352,10 +387,6 @@ class SnakeBase(object):
 
     def reduce_reward(self, value=1.0):
         self.reward -= value
-
-    def print_plat(self):
-        print(f"\n {self.plat}")
-        print(f"Score: {self.score}, reward: {self.reward}")
 
     def set_direction(self, direction: Direction):
         if self.snake_direction == Direction.UP:
@@ -420,10 +451,12 @@ class SnakeBase(object):
 
 
 if __name__ == "__main__":
-    sb = SnakeBase(width=5, height=5, bomb_num=5, food_num=5)
+    sb = SnakeBase(width=5, height=5, bomb_num=5, food_num=5, revive=True)
+    d = Direction.RIGHT
     for _ in range(10):
         sb.reset()
-        sb.snake_move(0, Direction.RIGHT)
+        sb.print_plat()
+        sb.snake_move(0, d)
         # sb.print_plat()
         # while not sb.terminal:
         #     sb.run_a_turn()
